11abecbd3ec58b084df4d2c00f3fdd9071017065
dvlpr2003 <gayathrigaya698@gmail.com>
2024-05-15 09:41:29 +0530

    -r

--- api/views.py
+++ api/views.py
@@ -16,40 +16,38 @@ class Instagram_Downloader(APIView):
             my_Url = url
             loader = instaloader.Instaloader()
             post = instaloader.Post.from_shortcode(loader.context, my_Url)
-            url_list = {
-
-            }
+            url_list = []
             if post._full_metadata['is_video']:
                 try :
 
                     x = post._full_metadata['edge_sidecar_to_children']["edges"]
                 except:
-                    url_list.update({"video_c5_img_cover1":post._full_metadata['display_url']})
-                    url_list.update({"video_c5_1":post._full_metadata['video_url']})
+                    url_list.extend([{"video_c5_img_cover1":post._full_metadata['display_url']}])
+                    url_list.extend([{"video_c5_1":post._full_metadata['video_url']}])
 
                     
 
                 else:
                     for i in range(len(x)):
                         if x[i]["node"]['is_video'] :
-                            url_list.update({f'video_c5_img_cover{i}':x[i]["node"]["display_resources"][0]["src"]})
-                            url_list.update({f'video_c5_{i}':x[i]["node"]["video_url"]})
+                            url_list.extend([{f'video_c5_img_cover{i}':x[i]["node"]["display_resources"][0]["src"]}])
+                            url_list.extend([{f'video_c5_{i}':x[i]["node"]["video_url"]}])
                             continue
-                        url_list.update({f'img_c5_{i}':x[i]["node"]["display_resources"][0]["src"]})
+                        url_list.extend([{f'img_c5_{i}':x[i]["node"]["display_resources"][0]["src"]}])
             else:
                 try :
 
                     x = post._full_metadata['edge_sidecar_to_children']["edges"]
                 except:
-                    url_list.update({"img_c5_1":post._full_metadata['display_url']})
+                    url_list.extend([{"img_c5_1":post._full_metadata['display_url']}])
                 else:
                     
                     for i in range(len(x)):
                         if x[i]["node"]['is_video'] :
-                            url_list.update({f'video_c5_img_cover{i}':x[i]["node"]["display_resources"][0]["src"]})
-                            url_list.update({f'video_c5_{i}':x[i]["node"]["video_url"]})
+                            url_list.extend([{f'video_c5_img_cover{i}':x[i]["node"]["display_resources"][0]["src"]}])
+                            url_list.extend([{f'video_c5_{i}':x[i]["node"]["video_url"]}])
                             continue
-                        url_list.update({f'img_c5_{i}':x[i]["node"]["display_resources"][0]["src"]})
+                        url_list.extend([{f'img_c5_{i}':x[i]["node"]["display_resources"][0]["src"]}])
             
             return Response(url_list)
 
